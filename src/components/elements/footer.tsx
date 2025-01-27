b484dda4b963463e507c5d914139d2b77337ba6a
Troy <me@troyhancock.com>
2025-01-27 09:21:13 -0800

    refactor footer component; enhance layout and add transition link

--- src/components/elements/footer.tsx
+++ src/components/elements/footer.tsx
@@ -2,12 +2,24 @@
 
 import React from 'react'
 
+import { TransitionLink } from "~/components/utils/transition-link"
+
 export default function footer() {
   return (
-    <footer className="relative w-full text-xs text-left font-sans text-gray-400">
-      <p className="container max-w-[1440px] mx-auto">
-        ©&nbsp;{new Date().getFullYear()}&nbsp;Made With ♡ by Rosie and Troy
-      </p>
+    <footer className="relative w-full text-xs text-left py-[3rem]">
+      <div className="container flex flex-col gap-6 max-w-[1440px] mx-auto">
+        <div>
+          <TransitionLink href="/" route="Home" className="flex flex-row items-center gap-4 text-3xl font-bold w-max">
+            <p className="uppercase">Rosie & Troy</p>
+            <p className="hidden sm:block font-sans font-medium text-lg pl-4 border-l-2">MAY 17, 2025</p>
+          </TransitionLink>
+
+        </div>
+        
+        <p className="font-sans text-gray-400">
+          ©&nbsp;{new Date().getFullYear()}&nbsp;Made With ♡ by Rosie and Troy
+        </p>
+      </div>  
     </footer>
   )
 }
